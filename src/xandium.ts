37bc8d426654f0ebf2f4fea0e043f467b9593ef1
Sharon <swilliams@xandium.io>
2019-10-09 00:41:16 -0400

    Added timer for status checks

--- src/xandium.ts
+++ src/xandium.ts
@@ -30,6 +30,12 @@ export class Xandium {
   }
 
   async run() {
+    this.statusTimer = setInterval(() => {
+      this.bots.forEach((value: Bot) => {
+        value.ws?.send('status');
+      });
+    }, 5000);
+
     this.mysqlConnection = mysql.createPool({
       host: this.opts.mysqlHost,
       user: this.opts.mysqlUser,
@@ -146,27 +152,27 @@ export class Xandium {
       host: this.opts.redisHost,
       port: this.opts.redisPort,
       password: this.opts.redisPass,
-        socket_keepalive: true,
+      socket_keepalive: true,
       retry_strategy: function(
         options: redis.RetryStrategyOptions
       ): number | Error {
-          if (options.error && options.error.code === "ECONNREFUSED") {
-            // End reconnecting on a specific error and flush all commands with
-            // a individual error
-            return new Error("The server refused the connection");
-          }
-          if (options.total_retry_time > 1000 * 60 * 60) {
-            // End reconnecting after a specific timeout and flush all commands
-            // with a individual error
-            return new Error("Retry time exhausted");
-          }
-          if (options.attempt > 10) {
-            // End reconnecting with built in error
-          return new Error("Reconnect attempt exceeded");
-          }
-          // reconnect after
-          return Math.min(options.attempt * 100, 3000);
+        if (options.error && options.error.code === "ECONNREFUSED") {
+          // End reconnecting on a specific error and flush all commands with
+          // a individual error
+          return new Error("The server refused the connection");
         }
+        if (options.total_retry_time > 1000 * 60 * 60) {
+          // End reconnecting after a specific timeout and flush all commands
+          // with a individual error
+          return new Error("Retry time exhausted");
+        }
+        if (options.attempt > 10) {
+          // End reconnecting with built in error
+          return new Error("Reconnect attempt exceeded");
+        }
+        // reconnect after
+        return Math.min(options.attempt * 100, 3000);
+      }
     };
     this.redis = redis.createClient(opts);
 
